98f4726269a1fae09aa11e3e3b4198a895a33b58
Adam Strickland <adam.strickland@gmail.com>
2025-10-01 12:13:56 -0400

    add kong for option parsing

--- cmd/daemonic/main.go
+++ cmd/daemonic/main.go
@@ -2,30 +2,32 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/adamstrickland/daemonic/pkg/daemon"
 	"github.com/adamstrickland/daemonic/pkg/example"
+	"github.com/alecthomas/kong"
 	"go.uber.org/zap"
 )
 
-var (
-	useZap = true
-)
-
-func init() {
-	flag.BoolVar(&useZap, "zap", false, "Use zap logger (default: slog)")
-
-	flag.Parse()
+var config struct {
+	UseZap bool `name:"zap" optional:"" help:"Use zap logger instead of slog."`
 }
 
 func main() {
+	k, err := kong.New(&config, kong.DefaultEnvars("MOM"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	k.Parse(os.Args[1:])
+
 	var logger daemon.Logger
 
-	if useZap {
+	if config.UseZap {
 		zlogger, err := zap.NewProduction()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "error creating logger: %v\n", err)
@@ -45,7 +47,6 @@ func main() {
 	}
 
 	app, err := example.NewTicker(example.WithLogger(logger))
-	// app, err := example.NewTicker()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
